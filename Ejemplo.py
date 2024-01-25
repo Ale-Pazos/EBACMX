215fd7a5a9ebac34d14cd13c2b642513a5a99909
Ale-Pazos <151708639+Ale-Pazos@users.noreply.github.com>
2024-01-24 19:14:15 -0600

    Update Ejemplo.py
    Añado la variable f

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,8 +1,10 @@
 #Se añadio la variable c
+#Añado la varibale f como factor de tendencia
 a=10
 b=30
-c=30*10*5
+f=5
+c=30*10*5/e*(1+f/100)
 print('resultado=',c)
 
-d=c*100
+d=c*200
 print('resultado=',d)

f8cf438c22a07c0c06950a2b3e0470b5a5dd0270
Ale-Pazos <alecapa90@hotmail.com>
2024-02-13 17:42:22 -0600

    Agregacion de G

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,10 +1,11 @@
 #Se añadio la variable c
-#Añado la varibale f como factor de tendencia
+#Añado la variable g como factor de tendencia
 a=10
 b=30
 e=1.1
 f=5
-c=30*10*5/e*(1+f/100)
+g=5
+c=g*30*10*5/e*(1+f/100)
 print('resultado=',c)
 
 d=c*200

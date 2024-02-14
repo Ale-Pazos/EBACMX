01e4ae73b2cd168f8e231d9959912e63507b3bf8
Ale-Pazos <alecapa90@hotmail.com>
2024-02-13 23:20:12 -0600

    Se quitan valores num de la ec C

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,11 +1,11 @@
 
-#Añadí la variable h como factor de tendencia
+#Se quitan valores numericos de la ecuacion C
 a=10
 b=30
 e=1.1
 f=5
 h=0.9
-c=h*30*10*5/e*(1+f/100)
+c=h*5/e*(1+f/100)
 print('resultado=',c)
 
 d=c*200

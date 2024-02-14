3a288321adfeaf5a2c1e782fc1cfbe8079a7039f
Ale-Pazos <alecapa90@hotmail.com>
2024-02-13 23:31:25 -0600

    Revert "no message"
    This reverts commit c556dcaeea4e801e01815eb6ab478aa8bd366c7a.

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,11 +1,11 @@
 
-#Se eliminaron valores numericos de la Ec. C
+#Añadí la variable h como factor de tendencia
 a=10
 b=30
 e=1.1
 f=5
 h=0.9
-c=h*5/e*(1+f/100)
+c=h*30*10*5/e*(1+f/100)
 print('resultado=',c)
 
 d=c*200

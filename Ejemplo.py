4b2923fc33e47a9c3c22baf19b0a80f061fd78b0
Ale-Pazos <alecapa90@hotmail.com>
2024-02-13 20:20:40 -0600

    Eliminacion de valores numericos en la Ec. C

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,11 +1,11 @@
-#Se añadio la variable c
-#Añado la variable g como factor de tendencia
+
+#Añadí la variable h como factor de tendencia
 a=10
 b=30
 e=1.1
 f=5
-g=5
-c=g*30*10*5/e*(1+f/100)
+h=0.9
+c=h*30*10*5/e*(1+f/100)
 print('resultado=',c)
 
 d=c*200
